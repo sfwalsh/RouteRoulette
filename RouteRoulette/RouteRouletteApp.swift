5298c4e799c02a55995b35f820705cb7e983f383
Stephen Walsh <sfwalshappdev@gmail.com>
2023-08-31 16:23:53 +0100

    Initial Commit

---
+++ RouteRoulette/RouteRouletteApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouteRouletteApp.swift
+//  RouteRoulette
+//
+//  Created by Stephen Walsh on 31/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct RouteRouletteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
