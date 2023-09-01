06ab0aed2da47223f6649cfcb14c0332445847f5
Stephen Walsh <sfwalshappdev@gmail.com>
2023-09-02 00:17:29 +0100

    added query for flights use case and tests; added datestringformatter and tests

--- RouteRoulette/Domain Layer/QueryBuilder.swift
+++ RouteRoulette/Domain Layer/QueryBuilder.swift
@@ -9,7 +9,7 @@ import Foundation
 
 enum QueryType {
     case place(searchTerm: String?)
-    case flight(sourceIdentifiers: [String], destinationIdentifiers: [String])
+    case flight(sourceIdentifiers: [String], destinationIdentifiers: [String], dateRangeBeginning: String, dateRangeEnd: String)
 }
 
 protocol QueryBuilder {
@@ -22,8 +22,13 @@ struct DefaultQueryBuilder: QueryBuilder {
         switch queryType {
         case .place(let searchTerm):
             return buildPlaceQuery(forSearchTerm: searchTerm)
-        case .flight(let sourceIdentifiers, let destinationIdentifiers):
-            return "" // todo
+        case .flight(let sourceIdentifiers, let destinationIdentifiers, let dateRangeBeginning, let dateRangeEnd):
+            return buildFlightQuery(
+                sourceIdentifiers: sourceIdentifiers,
+                destinationIdentifiers: destinationIdentifiers,
+                dateRangeBeginning: dateRangeBeginning,
+                dateRangeEnd: dateRangeEnd
+            )
         }
     }
     
@@ -49,5 +54,113 @@ struct DefaultQueryBuilder: QueryBuilder {
         }
         """
     }
-
+    
+    private func buildFlightQuery(
+        sourceIdentifiers: [String],
+        destinationIdentifiers: [String],
+        dateRangeBeginning: String,
+        dateRangeEnd: String
+    ) -> String {
+        // corner cutting: similar to above
+        let formattedSourceIdentifiers = format(for: sourceIdentifiers)
+        let formattedDestinationIdentifiers = format(for: destinationIdentifiers)
+        
+        return """
+        fragment stopDetails on Stop {
+          utcTime
+          localTime
+          station {
+            id
+            name
+            code
+            type
+            city {
+              id
+              legacyId
+              name
+              country {
+                id
+                name
+              }
+            }
+          }
+        }
+        
+        query onewayItineraries {
+          onewayItineraries(
+            filter: {allowChangeInboundSource: false, allowChangeInboundDestination: false, allowDifferentStationConnection: true, allowOvernightStopover: true, contentProviders: [KIWI], limit: 10, showNoCheckedBags: true, transportTypes: [FLIGHT]}
+            options: {currency: "EUR", partner: "skypicker", sortBy: QUALITY, sortOrder: ASCENDING, sortVersion: 4, storeSearch: true}
+            search: {cabinClass: {applyMixedClasses: true, cabinClass: ECONOMY}, itinerary: {source: {ids: \(formattedSourceIdentifiers)}, destination: {ids: \(formattedDestinationIdentifiers)}, outboundDepartureDate: {start: \(dateRangeBeginning), end: \(dateRangeEnd)}}, passengers: {adults: 1, adultsHandBags: [1], adultsHoldBags: [0]}}
+          ) {
+            ... on Itineraries {
+              itineraries {
+                ... on ItineraryOneWay {
+                  id
+                  duration
+                  cabinClasses
+                  priceEur {
+                    amount
+                  }
+                  bookingOptions {
+                    edges {
+                      node {
+                        bookingUrl
+                        price {
+                          amount
+                          formattedValue
+                        }
+                      }
+                    }
+                  }
+                  provider {
+                    id
+                    name
+                    code
+                  }
+                  sector {
+                    id
+                    duration
+                    sectorSegments {
+                      segment {
+                        id
+                        duration
+                        type
+                        code
+                        source {
+                          ...stopDetails
+                        }
+                        destination {
+                          ...stopDetails
+                        }
+                        carrier {
+                          id
+                          name
+                          code
+                        }
+                        operatingCarrier {
+                          id
+                          name
+                          code
+                        }
+                      }
+                      layover {
+                        duration
+                        isBaggageRecheck
+                        transferDuration
+                        transferType
+                      }
+                      guarantee
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+        """
+    }
+    
+    private func format(for items: [String]) -> String {
+        "[\"" + items.joined(separator: "\", \"") + "\"]"
+    }
 }
--- RouteRouletteTests/Domain Layer/QueryBuilderTests.swift
+++ RouteRouletteTests/Domain Layer/QueryBuilderTests.swift
@@ -19,44 +19,31 @@ final class QueryBuilderTests: XCTestCase {
     
     func testBuildPlaceQueryWithValidSearchTerm() {
         let searchTerm = "Paris CDG"
-        let expectedQueryString = expectedPlaceQuery(for: searchTerm)
-        
         let generatedQuery = sut.build(for: .place(searchTerm: searchTerm))
-        XCTAssertEqual(generatedQuery, expectedQueryString)
+        
+        XCTAssertTrue(generatedQuery.contains("places("))
+        XCTAssertTrue(generatedQuery.contains("term: \"\(searchTerm)\""))
     }
     
     func testBuildPlaceQueryWithNilSearchTerm() {
-        let expectedQueryString = expectedPlaceQuery(for: "")
-        
         let generatedQuery = sut.build(for: .place(searchTerm: nil))
-        XCTAssertEqual(generatedQuery, expectedQueryString)
+        
+        XCTAssertTrue(generatedQuery.contains("places("))
+        XCTAssertTrue(generatedQuery.contains("term: \"\""))
     }
     
     func testBuildFlightQuery() {
         let sourceIds = ["ID1", "ID2"]
         let destinationIds = ["DEST1", "DEST2"]
+        let startDate = "2023-07-01T00:00:00"
+        let endDate = "2023-07-07T23:59:59"
         
-        let generatedQuery = sut.build(for: .flight(sourceIdentifiers: sourceIds, destinationIdentifiers: destinationIds))
-        XCTAssertEqual(generatedQuery, "") // Currently it should return an empty string
-    }
-    
-    private func expectedPlaceQuery(for searchTerm: String) -> String {
-        """
-        query places {
-            places(
-                search: { term: "\(searchTerm)" },
-                filter: {
-                    onlyTypes: [AIRPORT, CITY],
-                    groupByCity: true
-                },
-                options: { sortBy: RANK },
-                first: 20
-            ) {
-                ... on PlaceConnection {
-                    edges { node { id legacyId name gps { lat lng } } }
-                }
-            }
-        }
-        """
+        let generatedQuery = sut.build(for: .flight(sourceIdentifiers: sourceIds, destinationIdentifiers: destinationIds, dateRangeBeginning: startDate, dateRangeEnd: endDate))
+        
+        XCTAssertTrue(generatedQuery.contains("onewayItineraries("))
+        XCTAssertTrue(generatedQuery.contains("ids: [\"ID1\", \"ID2\"]"))
+        XCTAssertTrue(generatedQuery.contains("ids: [\"DEST1\", \"DEST2\"]"))
+        XCTAssertTrue(generatedQuery.contains("start: \(startDate)"))
+        XCTAssertTrue(generatedQuery.contains("end: \(endDate)"))
     }
 }
